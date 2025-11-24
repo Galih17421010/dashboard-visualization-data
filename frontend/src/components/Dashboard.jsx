d6966180c4b33d0414f7a7ebc94d3381e83f2c6a
galihagussaputra <galih_agus@kinden.co.id>
2025-11-24 11:27:49 +0700

    Dashboard Component

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,3 +1,11 @@
+import { useRef, useState } from "react";
+
 const DataVisualizationDahsboard = () => {
-    const []
+    const [selectedYear, setSelectedYear] = useState('2025');
+    const [selectedCategory, setSelectedCategory] = useState('all');
+    const [selectedChart, setSelectedChart] = useState('revenue');
+
+    const barChartRef = useRef(null);
+    const lineChartRef = useRef(null);
+    const pieChartRef = useRef(null);
 }
